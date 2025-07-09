329fb75562bd357b9e8a906eca4709eb385eeb0d
한희태 <cnksncks0411@naver.com>
2025-07-09 16:00:48 +0900

    js 추가

---
+++ soomgo/javascript/func.js
@@ -0,0 +1,50 @@
+let list = ["a", "b", "c"];
+
+for(let i; i<list.length; i++) {
+
+}
+test.forEach((item) => 
+console.log(item)
+);
+
+let test = document.getElementsByClassName("star");
+
+test.forEach()
+
+let hInput = document.getElementById("h");
+
+
+
+hInput.addEventListener("click", asd());
+hInput.addEventListener("click", function test() {
+    
+});
+function asd(a, b, c) {
+
+}
+
+var aa = 1;
+
+$(".dynamic-button").click({
+    aa++;
+    if(aa === 5) {
+        $(".dynamic-button").addClass(".dynamic-button2");
+        $(".dynamic-button2").removeClass(".dynamic-button");
+    }
+});
+
+
+$(".dynamic-button2").click();
+
+function init(text) {
+    if(text.)
+}
+
+function fiveClick(event, class) {
+    
+
+    $(document).on(event, class, function() {
+        console.log("동적으로 추가된 버튼이 클릭됨");
+    });
+}
+
